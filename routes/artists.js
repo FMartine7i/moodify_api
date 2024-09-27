7613802befadca15de7828d02dfc4d7f854c9cc3
Jessica <jessiiescobar21@gmail.com>
2024-09-27 15:16:06 -0300

    cambio de las rutas de artistas

--- routes/artists.js
+++ routes/artists.js
@@ -2,8 +2,8 @@ const {Router} = require('express');
 const { getArtists, getArtistById, getArtistByGenre } = require('../controllers/artists');
 const router = Router();
 
-router.get('/artists', getArtists);
-router.get('/artists/:id', getArtistById);
-router.get('/artists/genre', getArtistByGenre); 
+router.get('/', getArtists);
+router.get('/:id', getArtistById);
+router.get('/genre/:genre', getArtistByGenre); 
 
 module.exports = router;
