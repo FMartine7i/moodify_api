41f2b0c4b974a329c948258c0e923c827da4732b
Federico Martinetti <frmmartinetti98@gmail.com>
2024-09-28 15:07:21 -0300

    rama mila mergeada

--- routes/albums.js
+++ routes/albums.js
@@ -1,13 +1,4 @@
 const { Router } = require('express')
-<<<<<<< HEAD
-const { getRecentRockAlbums, getMovieSoundtracks, getInstrumentalAlbums } = require('../controllers/albums')
-
-const router = Router()
-
-router.get('/recent-rock', getRecentRockAlbums)
-router.get('/movie-soundtracks', getMovieSoundtracks)
-router.get('/instrumental', getInstrumentalAlbums)
-=======
 const { getAlbums, getAlbumById, getAlbumsByYear, getInstrumentalAlbums } = require('../controllers/albums')
 const router = Router()
 
@@ -22,6 +13,5 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', getAlbumById)
->>>>>>> mila_branch
 
 module.exports = router
