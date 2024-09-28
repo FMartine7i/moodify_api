c9473b97ffaf217fb2e4f9c7542a7b2ffda451c9
Milagros Muñoz Nicosia <milagrosMn@users.noreply.github.com>
2024-09-27 22:20:58 -0300

    cambios q

--- routes/albums.js
+++ routes/albums.js
@@ -1,10 +1,7 @@
 const { Router } = require('express');
-const { getRecentRockAlbums, getMovieSoundtracks, getInstrumentalAlbums } = require('../controllers/album');
-
+const { getAlbums } = require('../controllers/albums');
 const router = Router();
 
-router.get('/albums/recent-rock', getRecentRockAlbums);
-router.get('/albums/movie-soundtracks', getMovieSoundtracks);
-router.get('/albums/instrumental', getInstrumentalAlbums);
+router.get('/', getAlbums);
 
 module.exports = router;
